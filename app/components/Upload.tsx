80bd276eda953bd1e85820e01a3024e4796bf240
MPG-3D <forgempg@gmail.com>
2026-05-14 23:55:22 +0200

    fix upload type error

--- app/components/Upload.tsx
+++ app/components/Upload.tsx
@@ -1,28 +1,23 @@
 "use client"
 
-import { UploadButton } from "@/app/utils/uploadthing"
+import { UploadButton } from "@uploadthing/react"
+import type { OurFileRouter } from "@/app/api/uploadthing/core"
 
 export default function Upload() {
   return (
     <div className="w-full">
-
-      <UploadButton
+      <UploadButton<OurFileRouter, "imageUploader">
         endpoint="imageUploader"
-
         onClientUploadComplete={() => {
           alert("Upload erfolgreich!")
         }}
-
         onUploadError={(error: Error) => {
           alert(error.message)
         }}
-
         appearance={{
-          button:
-            "bg-blue-600 hover:bg-blue-500 text-white px-8 py-4 rounded-xl",
+          button: "bg-blue-600 hover:bg-blue-500 text-white px-8 py-4 rounded-xl",
         }}
       />
-
     </div>
   )
 }
